019fbbb197bfeba9540ed632e31a794f590611d1
rafael <enterrafael@gmail.com>
2024-10-02 19:11:51 -0300

    maior numero de 2 e de 3

---
+++ Exercicios/MaiorMenor.py
@@ -0,0 +1,26 @@
+#Ler dois números
+number1 = int(input("Digite o primeiro número: "))
+number2 = int(input("Digite o segundo número: "))
+ 
+# Escolha o número maior
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number = number2
+ 
+# Imprimir o resultado
+print("O maior número é:", larger_number)
+
+
+# Outra maneira
+
+#Ler dois números
+number1 = int(input("\nDigite o primeiro número: "))
+number2 = int(input("Digite o segundo número: "))
+ 
+# Escolha o número maior
+if number1 > number2: larger_number = number1
+else: larger_number = number2
+ 
+# Imprimir o resultado
+print("O maior número é:", larger_number)
