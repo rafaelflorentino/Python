4041fb20c44ee760db507f4f9f77a21cad758938
rafael <enterrafael@gmail.com>
2024-11-01 18:55:40 -0300

    app flask, dataset aluguel, pandas

---
+++ Flask/app/aluno.py
@@ -0,0 +1,8 @@
+class Aluno:
+    def __init__(self, ra, nome, tempoestudo, rendafamiliar):
+        self.ra = ra #Registro do aluno (ra)
+        self.nome = nome # nome do aluno (nome)
+        self.tempoestudo = tempoestudo # Tempo de estudo (em horas)
+        self.rendafamiliar = rendafamiliar # Rendafamiliar do aluno (decimal)
+
+  
