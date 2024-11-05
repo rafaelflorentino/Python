6a02dfa929cee538f931c3d2d77d6bc6b6462d75
rafael <enterrafael@gmail.com>
2024-11-05 16:35:05 -0300

    imigrantes

--- IA/Imigrantes/imigrantes.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-
-app =  Flask(__name__)
-
-@app.route('/grafico')
-def mostrar_grafico():
-    return render_template('imigrantes_brasil.html')
-
-app.run(debug=True)
