dd4287446c45ac572eb3f665559e8949b240ec2c
rafael <enterrafael@gmail.com>
2024-11-12 17:46:33 -0300

    seniavox, wordcloud,vendas, regressão, imoveis, sentimento

---
+++ IA/Streamlit/carregar_arquivo.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Carregue o seu  DataSet")
+
+upload = st.file_uploader("Escolha o seu arquivo", type="csv")
+if upload is not None:
+    df = pd.read_csv(upload)
+    st.dataframe(df)
+    
+    
+# streamlit run carregar_arquivo.py
